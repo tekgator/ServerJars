c2421ef0ad210fec4d69283e348cfd730754421a
tekgator <info@tekgator.com>
2022-12-26 00:40:45 +1000

    Add Model JarDetailsExt to include Type, Category and DownloadUrl

---
+++ ServerJars/Models/JarDetailsExt.cs
@@ -0,0 +1,18 @@
+﻿namespace ServerJarsAPI.Models;
+
+public class JarDetailsExt : JarDetails
+{
+    public JarDetailsExt(JarDetails jarDetails)
+    {
+        Version = jarDetails.Version;
+        File = jarDetails.File;
+        Size = jarDetails.Size;
+        Md5 = jarDetails.Md5;
+        Built = jarDetails.Built;
+        Stability = jarDetails.Stability;
+    }
+
+    public string Type { get; set; } = string.Empty;
+    public string Category { get; set; } = string.Empty;
+    public string DownloadUrl { get; set; } = string.Empty;
+}
